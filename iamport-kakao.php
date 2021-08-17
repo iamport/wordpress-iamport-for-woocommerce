a2d3dd256708412ee352e2e6596a5214781a278a
hiddenpunch <gideok@Racoon-EX-MAC.local>
2021-08-17 19:32:18 +0900

    erase log

--- iamport-kakao.php
+++ iamport-kakao.php
@@ -177,7 +177,6 @@ class WC_Gateway_Iamport_Kakao extends Base_Gateway_Iamport {
 
 		global $wpdb;
 
-		error_log('######## SCHEDULED ########', 3, "/var/log/apache2/php_error.log");
 		$creds = $this->getRestInfo(null, false); //this->imp_rest_key, this->imp_rest_secret사용하도록
 		$customer_uid = IamportHelper::get_customer_uid( $renewal_order );
 		$response = $this->doPayment($creds, $renewal_order, $amount_to_charge, $customer_uid, $renewal_order->suspension_count );
